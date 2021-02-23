be61758eb8e3879e8a3d19b9fc1a4a86189f08d8
Tenzin Sean <tenzin.jeunesse@gmail.com>
2021-02-23 01:31:47 +0100

    Add file create and edit
    * Add files and create

---
+++ routes/items.go
@@ -0,0 +1,25 @@
+package routes
+
+import (
+	"net/http"
+	"offersapp/models"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4"
+)
+
+func ItemsCreate(c *gin.Context) {
+	userID := c.GetString("user_id")
+	db, _ := c.Get("db")
+	conn := db.(pgx.Conn)
+
+	item := models.Item{}
+	c.ShouldBindJSON(&item)
+	err := item.Create(&conn, userID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+
+		c.JSON(http.StatusOK, item)
+	}
+}
